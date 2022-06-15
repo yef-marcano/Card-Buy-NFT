392f8f21c26301e08bdeaeba12cbeb4a732c7ac5
Yeferson Marcano <yefelex10@gmail.com>
2022-06-15 18:54:07 -0300

    Mejora de estructura al dar clic eliminada la función al abrir el index
    Se elimino la conexión en la carga para evitar conectar a metamask innecesariamente solo si se da conectar y luego pagar el NFT.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,44 @@
 window.addEventListener('load', async () => {
+  // Boton de conectar
+  $('.connect-button').click(async () => {
     if (window.ethereum) {
       window.web3 = new Web3(ethereum);
       try {
         await ethereum.enable();
         initPayButton()
       } catch (err) {
-        $('#status').html('Usuario denegado el acceso a la cuenta', err)
+        $('.status').html('Acceso denegado a la cuenta', err)
       }
     } else if (window.web3) {
       window.web3 = new Web3(web3.currentProvider)
       initPayButton()
     } else {
-      $('#status').html('No se ha instalado Metamask (u otro proveedor Web3)')
+      $('.status').html('No se ha instalado Metamask (u otro proveedor Web3)')
     }
   })
+})
 
-  const initPayButton = () => {
-    $('.pay-button').click(() => {
-      // La dirección de pago es donde se enviarán los fondos.
-      const paymentAddress = '0xA987A265b7438158d1598a47b84102EA9CF0F2e5'
-      const amountEth = 0.041
 
-      web3.eth.sendTransaction({
-        to: paymentAddress,
-        value: web3.toWei(amountEth, 'ether')
-      }, (err, transactionId) => {
-        if (err) {
-          console.log('Pago fallido', err)
-          $('#status').html('Pago fallido')
-        } else {
-          console.log('Payment successful', transactionId)
-          $('#status').html('Pago exitoso')
-        }
-      })
+const initPayButton = () => {
+  $("#pay-connect").html('Pagar').addClass('pay-button').removeClass('connect-button');
+  // Boton de pagar
+  $('.pay-button').click(() => {
+    // La dirección de pago es donde se enviarán los fondos.
+    const paymentAddress = '0xA987A265b7438158d1598a47b84102EA9CF0F2e5'
+    const amountEth = 0.041
+
+    web3.eth.sendTransaction({
+      to: paymentAddress,
+      value: web3.toWei(amountEth, 'ether')
+    }, (err, transactionId) => {
+      if (err) {
+        console.log('Pago fallido', err)
+        $('.status').html('Pago fallido')
+      } else {
+        console.log('Payment successful', transactionId)
+        $('.status').html('Pago exitoso')
+      }
     })
+  })
 
 }
